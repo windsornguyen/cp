65bc7982fdf5d1467a76b8081bb52d7dfc57f124
Windsor Nguyen <93564933+windsornguyen@users.noreply.github.com>
2023-01-27 11:14:47 -0600

    init

---
+++ template.cpp
@@ -0,0 +1,27 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+typedef long long ll;
+typedef vector<int> vi; // int vector
+typedef pair<int,int> pi; // int pair
+using namespace std;
+using ll = long long;
+using ui = unsigned int;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+const int inf = 1e9;
+const ll inf64 = 1e18;
+#define F first
+#define S second
+#define back push_back
+#define mp make_pair
+
+int main() {
+	ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tt;
+  cin >> tt;
+  
+} 
